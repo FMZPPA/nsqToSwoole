fe34fa6220a72600cde1bc16f18baa708e119b8f
谢磊 <443833189@qq.com>
2017-08-15 10:41:34 +0800

    test

--- autoload.php
+++ autoload.php
@@ -3,7 +3,6 @@
 function classLoader($class) {
     $path = str_replace(['\\', 'Iris/NsqToSwoole/'], [DIRECTORY_SEPARATOR, ''], $class);
     $file = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $path . '.php';
-    var_dump($file);
     if (file_exists($file)) {
         require_once $file;
     }
